eb238f6a096144ec5acbd49ff66180c36183bcea
NET14_NgocQuan <quantnse170175@fpt.edu.vn>
2025-10-14 17:37:30 +0700

    Revert "Merge pull request #3 from Quan170175/quan"
    This reverts commit ea61742553b3b4bbe139aabf11b2139c4e9c4ca5, reversing
    changes made to cc27d921fcf077b250cf3e1f4454eac59f9e323f.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,22 +1,22 @@
-// import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-// import { ThemedText } from '@/components/themed-text';
-// import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
 
-// export default function ProfileScreen() {
-//   return (
-//     <ThemedView style={styles.container}>
-//       <ThemedText type="title">Profile</ThemedText>
-//       <ThemedText>Đây là trang Profile của bạn</ThemedText>
-//     </ThemedView>
-//   );
-// }
+export default function ProfileScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Profile</ThemedText>
+      <ThemedText>Đây là trang Profile của bạn</ThemedText>
+    </ThemedView>
+  );
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 20,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
